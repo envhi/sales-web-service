69445479fa3207ce609a22a26888f3e20090482d
Felipe Fonseca <felipeaf196@gmail.com>
2025-03-27 21:22:29 -0300

    clean migrations

--- src/db/migrations/20250326173322_setup_db.js
+++ src/db/migrations/20250326173322_setup_db.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function (knex) {
 
     return knex.raw(
@@ -56,10 +52,6 @@ exports.up = function (knex) {
 
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function (knex) {
     return knex.raw(
         `     
